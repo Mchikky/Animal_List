8aab267e63b87e75320388f5df2d32092f700c5c
Chikaodili Deng <mchikky@yahoo.com>
2023-03-23 14:32:33 +0100

    Initial Commit

---
+++ Animal_List/Animal_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_ListApp.swift
+//  Animal_List
+//
+//  Created by Chikaodili Deng on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Animal_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
